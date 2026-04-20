59e717403bf23113fc655284d482fcb8844b3595
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2026-04-20 20:47:49 +0200

    Revert Clang work-arounds

--- src/proxy.cpp
+++ src/proxy.cpp
@@ -1,11 +1,3 @@
-// Work around Clang quirk: https://github.com/llvm/llvm-project/issues/86077
-// This must be included before `<exception>` to work correctly, so effectively before any standard library headers.
-#if defined( __APPLE__ ) && defined( __arm64__ )
-#include <version>
-#undef _LIBCPP_AVAILABILITY_HAS_INIT_PRIMARY_EXCEPTION
-#define _LIBCPP_AVAILABILITY_HAS_INIT_PRIMARY_EXCEPTION 0
-#endif
-
 #include "fastmcpp/proxy.hpp"
 
 #include "fastmcpp/client/transports.hpp"
